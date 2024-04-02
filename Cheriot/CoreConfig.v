e490da937f52b7b8cfbb77d39045acb8d3b6900a
Murali Vijayaraghavan <muralivi@google.com>
2024-04-01 20:32:51 -0700

    fixing a bunch of issues

--- Cheriot/CoreConfig.v
+++ Cheriot/CoreConfig.v
@@ -21,31 +21,34 @@ Section Prefix.
   Definition specRegs : list RegInitT :=
     (@^pcCapReg, existT _ (SyntaxKind Cap) (Some (SyntaxConst (convTypeToConst pcCapInit)))) ::
       (@^pcValReg, existT _ (SyntaxKind Addr) (Some (SyntaxConst (wcombine pcValInit (wzero 2))))) ::
-      (@^regsArray, existT _ _ (Some (SyntaxConst (@convTypeToConst (Array 32 FullCapWithTag) regsInit)))) ::
+      (@^regsArray, existT _ _ (Some (SyntaxConst (@convTypeToConst (Array NumRegs FullCapWithTag) regsInit)))) ::
       (@^memArray, existT _ _ (Some (SyntaxConst (@convTypeToConst (Array (NumMemBytes * NumBanks) (Bit 8))
                                                     memInit)))) ::
       (@^tagArray, existT _ _ (Some (SyntaxConst (ConstArray (fun (i: Fin.t NumMemBytes) => false))))) ::
       if hasTrap
       then
         (@^mtccReg,
-          existT _ _ (Some (SyntaxConst
-                              (convTypeToConst
-                                 (evalExpr (STRUCT { "tag" ::= Const type true;
-                                                     "cap" ::= ExecRootCapExpr type;
-                                                     "val" ::= Const _ (wcombine mtccValInit (wzero 2))})))))) ::
+          existT _ (SyntaxKind FullCapWithTag)
+            (Some (SyntaxConst
+                     (convTypeToConst
+                        (evalExpr (STRUCT { "tag" ::= Const type true;
+                                            "cap" ::= ExecRootCapExpr type;
+                                            "val" ::= Const _ (wcombine mtccValInit (wzero 2))})))))) ::
           (@^mtdcReg,
-            existT _ _ (Some (SyntaxConst
-                                (convTypeToConst
-                                   (evalExpr (STRUCT { "tag" ::= Const type true;
-                                                       "cap" ::= DataRootCapExpr type;
-                                                       "val" ::= Const _ (wcombine mtdcValInit (wzero 2))})))))) ::
+            existT _ (SyntaxKind FullCapWithTag)
+              (Some (SyntaxConst
+                       (convTypeToConst
+                          (evalExpr (STRUCT { "tag" ::= Const type true;
+                                              "cap" ::= DataRootCapExpr type;
+                                              "val" ::= Const _ (wcombine mtdcValInit (wzero 2))})))))) ::
           (@^mScratchCReg,
-            existT _ _ (Some (SyntaxConst
-                                (convTypeToConst
-                                   (evalExpr (STRUCT { "tag" ::= Const type true;
-                                                       "cap" ::= SealRootCapExpr type;
-                                                       "val" ::= Const _ (wzero Xlen)})))))) ::
-          (@^mepccReg, existT _ _ (Some (SyntaxConst (getDefaultConst FullCapWithTag)))) ::
+            existT _ (SyntaxKind FullCapWithTag)
+              (Some (SyntaxConst
+                       (convTypeToConst
+                          (evalExpr (STRUCT { "tag" ::= Const type true;
+                                              "cap" ::= SealRootCapExpr type;
+                                              "val" ::= Const _ (wzero Xlen)})))))) ::
+          (@^mepccReg, existT _ (SyntaxKind FullCapWithTag) (Some (SyntaxConst (getDefaultConst FullCapWithTag)))) ::
           (@^mStatusReg, existT _ (SyntaxKind Data) (Some (SyntaxConst (wzero Xlen)))) ::
           (@^mieReg, existT _ (SyntaxKind Data) (Some (SyntaxConst (wzero Xlen)))) ::
           (@^mCauseReg, existT _ (SyntaxKind Data) None) ::
--- Cheriot/Tactics.v
+++ Cheriot/Tactics.v
@@ -17,6 +17,24 @@ Ltac dischargeWfActionT :=
   | |- _ -> _ => intros 
   end.
 
+Ltac dischargeWfActionT_specific :=
+  match goal with
+  | |- @WfActionT _ _ _ (convertLetExprSyntax_ActionT ?e) => apply WfLetExprSyntax_regs
+  | |- @WfActionT _ _ _ (MCall _ _ _ _) => apply WfMCall
+  | |- @WfActionT _ _ _ (LET _ <- _; _)%kami_action => apply WfLetExpr
+  | |- @WfActionT _ _ _ (LETA _ <- _; _)%kami_action => apply WfLetAction
+  | |- @WfActionT _ _ _ (NondetN _ : _; _)%kami_action => apply WfReadNondet
+  | |- @WfActionT _ ?regs _ (Read _ <- ?r; _)%kami_action => apply WfReadReg;
+                                                             [| let y := getOrIntrosLtacHelp r regs in exact y]
+  | |- @WfActionT _ ?regs _ (Write ?r <- _; _)%kami_action => apply WfWriteReg;
+                                                              [| let y := getOrIntrosLtacHelp r regs in exact y]
+  | |- @WfActionT _ _ _ (If _ then _ else _ as _; _)%kami_action => apply WfIfElse
+  | |- @WfActionT _ _ _ (System ?ls; _)%kami_action => apply WfSys
+  | |- @WfActionT _ _ _ (Ret _)%kami_action => apply WfReturn
+  | |- forall _, _ => intros
+  | |- _ -> _ => intros 
+  end.
+
 Ltac getOrIntrosLtac :=
   match goal with
   | |- In (?val, _) (getKindAttr ?ls) =>
