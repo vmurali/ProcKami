8735bd94d2f7eb19ea6df1e3c64e44fceaf538fb
Murali Vijayaraghavan <muralivi@google.com>
2024-04-06 23:41:48 -0700

    fixed IsolationSpec, except for initializing memory

--- Cheriot/IsolationSpec.v
+++ Cheriot/IsolationSpec.v
@@ -33,7 +33,21 @@ Section InBoundsPermsTy.
         LETC permsSub <- isSubsetPerms #perms1 #perms2;
         RetE (#permsSub && (#range1 @% "base" >= #range2 @% "base") && (#range1 @% "top" <= #range2 @% "top"))).
   End Subset.
+
+  Definition hasRead (cap: Cap @# ty): Bool @# ty := getCapPerms cap @% "LD".
+  Definition hasWrite (cap: Cap @# ty): Bool @# ty := getCapPerms cap @% "SD".
+  Definition hasMC (cap: Cap @# ty): Bool @# ty := getCapPerms cap @% "MC".
+
+  Definition CurrPlusSizeInBounds (cap: FullCap @# ty) sz : Bool ## ty :=
+    ( LETE range : _ <- getCapBaseTop cap;
+      LETC baseBound : Bool <- #range @% "base" >= cap @% "val";
+      LETC topBound : Bool <- (ZeroExtend 1 (cap @% "val") + $sz) <= #range @% "top";
+      RetE (#baseBound && #topBound )).
+
 End InBoundsPermsTy.
+
+Definition CurrPlusSizeInBoundsProp (cap: type FullCap) sz :=
+  evalLetExpr (CurrPlusSizeInBounds ###cap sz)%kami_expr = true.
 
 Section Dominating.
   Local Open Scope kami_expr.
@@ -45,16 +59,20 @@ Section Dominating.
                               LETE isSubsetCap <- SubsetCap (rmTag ###testCap) ###dc;
                               RetE (!(###testCap @% "tag") || ###isSubsetCap))) l = true.
 
-  Definition DominatingCapsPc (l: list (type FullCap)) (pcc: type FullCap) :=
-    existsb (fun dc => evalLetExpr (SubsetCap ###pcc ###dc)) l = true.
+  Definition DominatingCapsSingle (l: list (type FullCap)) (cap: type FullCap) :=
+    existsb (fun dc => evalLetExpr (SubsetCap ###cap ###dc)) l = true.
     
   Definition DominatingCapsOverlap (l1 l2: list (type FullCap)) :=
     exists (addr: type Addr), (existsb (fun c => evalLetExpr (InBoundsPermsAddr ###addr ###c)) l1) = true /\
                                 (existsb (fun c => evalLetExpr (InBoundsPermsAddr ###addr ###c)) l2) = true.
 End Dominating.
 
+Definition DominatingCapsNoOverlap (l1 l2: list (type FullCap)) := ~ (DominatingCapsOverlap l1 l2).
+
 Section IsolationSpec.
   Local Open Scope kami_expr.
+
+  Local Notation MemVar x := (Var type (SyntaxKind (Array _ FullCapWithTag)) x).
 
   Record IsolationSpec := {
       cores: list (CoreConfigParams * list (type FullCap));
@@ -67,7 +85,7 @@ Section IsolationSpec.
       coreDominatingRegCaps: forall c, In c cores -> DominatingCaps (snd c) (@regsInit (fst c));
       coreDominatingPcc: forall c,
         In c cores ->
-        DominatingCapsPc (snd c)
+        DominatingCapsSingle (snd c)
           (evalExpr (STRUCT { "cap" ::= Const _ (convTypeToConst (@pcCapInit (fst c)));
                               "val" ::= Const _ (wcombine (@pcValInit (fst c)) (wzero 2))}));
       coreNoTrap: forall c, In c cores -> @hasTrap (fst c) = false;
@@ -76,23 +94,60 @@ Section IsolationSpec.
                                  @LgNumMemBytes (@memParams (fst c)) = @LgNumMemBytes (@memParams trapCore);
       sameMemTrapCore: forall x c dc,
         In c cores -> In dc (snd c) -> evalLetExpr (InBoundsAddr (###x) (###dc)) = true ->
-        evalExpr (Var type (SyntaxKind (Array _ FullCapWithTag))(@memInit (@memParams trapCore)) @[###x]) =
-          evalExpr (Var type (SyntaxKind (Array _ FullCapWithTag))(@memInit (@memParams (fst c))) @[###x]);
+        evalExpr (MemVar (@memInit (@memParams trapCore)) @[###x]) =
+          evalExpr (MemVar (@memInit (@memParams (fst c))) @[###x]);
       trapCoreHasTrap: @hasTrap trapCore = true;
-      mtccValidThm: MtccValid (@mtccCap trapCore) (wcombine (@mtccVal trapCore) (wzero 2)) trapHandlerSize;
-      mtdcValidThm: MtdcValid (@mtdcCap trapCore) (wcombine (@mtdcVal trapCore) (wzero 3))
-                      (MtdcTotalSize numCoresWord)
+      mtccValXlen := wcombine (@mtccVal trapCore) (wzero 2) : type Addr;
+      mtccValidThm: MtccValid (@mtccCap trapCore) mtccValXlen trapHandlerSize;
+      mtdcValXlen := wcombine (@mtdcVal trapCore) (wzero 3) : type Addr;
+      mtdcValidThm: MtdcValid (@mtdcCap trapCore) mtdcValXlen (MtdcTotalSize numCoresWord);
+      mtccFullCap := evalExpr (STRUCT { "cap" ::= ###(@mtccCap trapCore);
+                                        "val" ::= ###mtccValXlen });
+      mtdcFullCap := evalExpr (STRUCT { "cap" ::= ###(@mtdcCap trapCore);
+                                        "val" ::= ###mtdcValXlen });
+      curr := (MemVar (@memInit (@memParams trapCore))) @[###mtdcValXlen + $16];
+      currTagged: evalExpr (curr @% "tag") = true;
+      currCapIsMtdcCap: evalExpr (curr @% "cap") = @mtdcCap trapCore;
+      currAddr: exists n, (n < wordVal _ numCoresWord)%Z /\
+                            evalExpr (curr @% "val") =
+                              wadd mtdcValXlen (ZToWord Xlen (24 + (n * (Z.of_nat NumRegs * 8))));
+      currReadWriteMc: evalExpr (hasRead (rmTag curr @% "cap") && hasWrite (rmTag curr @% "cap") &&
+                                   hasMC (rmTag curr @% "cap")) = true;
+      mtdcDominatesCurr: DominatingCapsSingle [mtdcFullCap] (evalExpr (rmTag curr));
+      mTimeCap: type FullCap;
+      mTimeCmpCap: type FullCap;
+      mTimeCapEq: mTimeCap = (evalExpr (rmTag ((MemVar (@memInit (@memParams trapCore))) @[###mtdcValXlen])));
+      mTimeCmpCapEq: mTimeCmpCap =
+                       (evalExpr (rmTag ((MemVar (@memInit (@memParams trapCore))) @[###mtdcValXlen + $8])));
+      mTimeSize: CurrPlusSizeInBoundsProp mTimeCap 8;
+      mTimeCmpSize: CurrPlusSizeInBoundsProp mTimeCmpCap 4;
+      mTimeRead: evalExpr (hasRead (###mTimeCap @% "cap")) = true;
+      mTimeCmpReadWrite: evalExpr (hasRead (###mTimeCap @% "cap") && hasWrite (###mTimeCmpCap @% "cap")) = true;
+      mtdcCapReadWriteMc: evalExpr (hasRead ###(@mtdcCap trapCore) && hasWrite ###(@mtdcCap trapCore) &&
+                                      hasMC ###(@mtdcCap trapCore)) = true;
+      disjointCaps: ForallOrdPairs DominatingCapsNoOverlap
+                      ([mtccFullCap] :: [mtdcFullCap] :: [mTimeCap] :: [mTimeCmpCap] :: map snd cores)
     }.
 End IsolationSpec.
 
-(*
-- Dominating caps are all disjoint
-- mtcc and mtdc are disjoint from any of the dominating caps
-- mtcc and mtdc are disjoint from each other
-- curr is dominated by mtdc
-- mtimeAddr is disjoint from others, including mtcc, mtdc and mtimeCmpAddr
-- mtimeCmpAddr is disjoint from others, including mtcc, mtdc and mtimeCmpAddr
-- mtimeAddr can hold 8 bytes
-- mtimeCmpAddr can hold 4 bytes
-- Initialize memory with trap handler (at mtcc) and trap data (at mtdc)
+Theorem test1 A (P: A -> Prop) : ~ (exists a, P a) -> (forall a, ~ P a).
+Proof.
+  intros.
+  intro.
+  assert (exists a, P a) by (exists a; auto).
+  tauto.
+Qed.
+
+Theorem test2 A (P: A -> Prop) : (forall a, ~ P a) -> ~ (exists a, P a).
+Proof.
+  intros.
+  intro.
+  destruct H0.
+  specialize (H x).
+  tauto.
+Qed.
+  
+
+  (*
+ Initialize memory with trap handler (at mtcc) and trap data (at mtdc)
  *)
